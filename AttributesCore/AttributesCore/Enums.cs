7fc9aa80eeb5f322272901f13e7ba1afa836bb4f
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-07-18 00:30:48 -0400

    Added enum named value attributes

---
+++ AttributesCore/AttributesCore/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rem.Attributes;
+
+/// <summary>
+/// Indicates that an <see langword="enum"/>-type property, field or return value should not accept values of its
+/// type that are unnamed values.
+/// </summary>
+/// <remarks>
+/// This attribute can also be used to indicate that a property, field or return value of an <see langword="enum"/>
+/// type that is decorated with a <see cref="FlagsAttribute"/> should only accept a bit set of named values.
+/// </remarks>
+[AttributeUsage(
+    AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.ReturnValue,
+    AllowMultiple = false,
+    Inherited = true)]
+public sealed class NamedEnumAttribute : Attribute { }
+
+/// <summary>
+/// Indicates that an <see langword="enum"/>-type parameter should be a named value of its type.
+/// </summary>
+/// <remarks>
+/// This attribute can also be used to indicate that a parameter of an <see langword="enum"/>
+/// type that is decorated with a <see cref="FlagsAttribute"/> should be a bit set of only named values.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+public sealed class NamedEnumParameterAttribute : Attribute { }
+
