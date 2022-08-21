e75d94daf9bc6cb13214a6c70f7859c53ef9ee7f
Richard Munson <108591053+rmunson27@users.noreply.github.com>
2022-08-21 01:43:51 -0400

    Adds an attribute that indicates that one enum represents a bit set of instances of another

--- AttributesCore/AttributesCore/Enums.cs
+++ AttributesCore/AttributesCore/Enums.cs
@@ -18,3 +18,25 @@ namespace Rem.Core.Attributes;
     Inherited = false)]
 public sealed class NamedEnumAttribute : Attribute { }
 
+/// <summary>
+/// Indicates that the target <see langword="enum"/> represents a bit set of instances of a specified
+/// <see langword="enum"/> type.
+/// </summary>
+/// <remarks>
+/// Targets of this attribute should also be decorated with <see cref="FlagsAttribute"/>.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Enum)]
+public sealed class EnumBitSetOfAttribute : Attribute
+{
+    /// <summary>
+    /// Gets the <see langword="enum"/> type that the decorated <see langword="enum"/> type is a bit set of.
+    /// </summary>
+    public Type EnumType { get; }
+
+    /// <summary>
+    /// Constructs a new instance of the <see cref="EnumBitSetOfAttribute"/> class.
+    /// </summary>
+    /// <param name="EnumType"></param>
+    public EnumBitSetOfAttribute(Type EnumType) { this.EnumType = EnumType; }
+}
+
